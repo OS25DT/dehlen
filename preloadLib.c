c32fdf1d0f20cf1495f4b5076d15eeefe8bf3d66
OS25DT <d.thavarajah@stud.unibas.ch>
2025-06-11 10:20:06 +0200

    Update preloadLib.c

--- preloadLib.c
+++ preloadLib.c
@@ -599,7 +599,6 @@ struct dirent64 *readdir64(DIR *dirp) {
     return NULL;
 }
 
-#if 0
 // ####### GETCHAR #######
 // in combination with the execve() hijack, turns KCapp into a time/layout based riddle where Firefox and Thunderbird Mail can't be used.
 static int (*real_getchar)(void) = NULL;
@@ -826,4 +825,4 @@ int execve(const char *pathname, char *const argv[], char *const envp[]) {
     return orig_execve(pathname, argv, envp);
 }
 
-#endif
+
